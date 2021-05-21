54d6e7fc7450e44be4cea502ddb51dcd5de9bb0a
rin1782 <colaerin@yahoo.com>
2021-05-21 11:31:03 -0400

    working on DOM

--- src/index.js
+++ src/index.js
@@ -1,40 +1,93 @@
 const beerForm = document.getElementById("beerForm")
 const beerInput = document.getElementById("beerInput")
 const beerList = document.getElementById("beerList")
+const beerURL = "http://localhost:3000/beers"
+const reviewURL = "http://localhost:3000/reviews"
 
-beerForm.addEventListener("submit", submitBeer)
 
-function submitBeer(e){
-    e.preventDefault()
-    console.log(beerInput.value)
-    const li = document.createElement("li")
+function fetchBeers(){
+    fetch(beerURL)
+    .then(Response => Response.json())
+    .then(beers => beers.forEach(displayBeer))
+
+}
+   
+
     
-    const h3 = document.createElement("h3")
-    h3.innerText = beerInput.value
+    // .then(beers => beers.forEach(beer => displayBeer(beer.content)))
+    
 
+    beerForm.addEventListener("submit", submitBeer)
+
+function submitBeer(){
+    event.preventDefault()
+    const configObj= {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: beer.name
+        })
+    }
+    
+        fetch(beerURL, configObj)
+        .then(r => r.json())
+        .then(displayBeer)
+        
+}
+
+
+function displayBeer(beer){
+    const li = document.createElement("li")
+    li.dataset.id = beer.id
+
+    const h3 = document.createElement("h3")
+    h3.innerText = beer.name
+    console.log(beer)
     const reviewForm = document.createElement("form")
     reviewForm.innerHTML += `
         <input type="text" id="reviewInput">
         <input type="submit">
         `
-        reviewForm.addEventListener("submit", submitReview)
+        reviewForm.addEventListener("submit", renderReview)
 
         const reviewList = document.createElement("ul")
 
 
         li.append(h3, reviewForm, reviewList)
-
         beerList.appendChild(li)
 
         beerForm.reset()
 }
 
-    function submitReview(e){
+    function renderReview(e){
         e.preventDefault()
         const reviewInput = e.target.children[0].value
         const reviewList = e.target.nextElementSibling
 
         const li = document.createElement("li")
         li.innerText = reviewInput
-        console.log(e.target)
+        reviewList.appendChild(li)
+
+        submitReview(reviewInput)
+
+        e.target.reset()
+    }
+
+    function submitReview(review){
+        fetch(reviewURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "accept": "application/json"
+            },
+            body: JSON.stringify({
+                content: review
+            })
+        })
+    
 }
+
+fetchBeers()
