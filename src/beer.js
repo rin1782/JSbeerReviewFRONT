b4ee2402732558b6ded6aa045d53e07663b739df
rin1782 <colaerin@yahoo.com>
2021-05-23 15:00:01 -0400

    finish

--- src/beer.js
+++ src/beer.js
@@ -1,72 +1,55 @@
+const baseURL = "http://localhost:3000/"
+const beerContainer = document.querySelector("#beers-list")
+const beerPage = document.querySelector("#beer-show")
+
+
 class Beer {
     static all = []
     constructor({id, name}){
-        this.id = id;
-        this.name = name;
+        this.id = id
+        this.name = name
         Beer.all.push(this)
     }
 
     static fetchBeers(){
-        fetch(beerURL)
-        .then(r => r.json())
-        .then(data => {
-            data.forEach(b => {
-                let beer = new Beer(b)
-                beer.showBeer()
+        fetch(baseURL + "beers")
+        .then(res => res.json())
+        .then(beerInfo => {
+            beerInfo.forEach(beer => {
+                let newBeer = new Beer(beer)
+                newBeer.addToDom()
             })
         })
     }
-    
-        showBeer(){
-        const h3 = document.createElement("h3")
-        const ul = document.createElement("ul")
-        const reviewForm = document.createElement("form")
-        reviewForm.innerHTML += `
-            <input type="text" id="reviewInput">
-            <input type="submit">
-        `
-        reviewForm.addEventListener("submit", (e) => {renderReview()})
 
-        const reviewList = document.createElement("ul")
+    handleClick(e){
+        beerContainer.style.display="none"
+        beerPage.style.display=""
+        this.addToBeerPage()
+        let beerID = parseInt(e.target.id.split("-")[1])
+        Review.fetchReviews(beerID)
 
-        
-        h3.id = `beer-${this.id}`
-        h3.innerText = this.name
-
-        
-        
-        
-        
-        ul.append(h3, reviewForm, reviewList)
-        beerList.appendChild(ul)
     }
 
-        renderReview(e){
-        e.preventDefault()
-        const reviewInput = e.target.children[0].value
-        const reviewList = e.target.nextElementSibling
-
-        const li = document.createElement("li")
-        li.innerText = reviewInput
-        reviewList.appendChild(li)
-
-        submitReview(reviewInput)
-
-        e.target.reset()
+    addToDom(){
+        let beerName = document.createElement('li');
+        beerName.innerText = this.name
+        beerName.id = `beer-${this.id}`
+        beerContainer.appendChild(beerName)
+        beerName.addEventListener('click',(e) => this.handleClick(e))
     }
 
-        submitReview(review){
-        fetch(reviewURL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "accept": "application/json"
-            },
-            body: JSON.stringify({
-                content: review
-            })
-        })
+    addToBeerPage(){
+        let div = document.createElement('div');
+        let beerLi = document.createElement('h2');
+        beerLi.innerText = this.name
+        div.appendChild(beerLi)
+        beerPage.prepend(div)
+        let input = document.createElement('input')
+        input.type="hidden"
+        input.value= this.id
+        input.id="beer_id"
+        reviewForm.prepend(input)
+    }
 
-        
-
-    }}
+}
---
+++ src/review.js
@@ -0,0 +1,78 @@
+const newReview = document.querySelector("#new-review")
+const reviewForm = document.querySelector("#review-form")
+
+class Review {
+    constructor({id, content}){
+        this.content = content
+
+        this.element = document.createElement("div")
+        this.element.id = `review-${id}`
+        this.reviewsList = document.querySelector("#reviews-list")
+    
+      }
+
+    static fetchReviews(id){
+
+        fetch(`http://localhost:3000/beers/${id}/reviews`)
+        .then(res => res.json())
+        .then(reviewInfo => {
+                reviewInfo.forEach(review => {
+                    let r = new Review(review)
+                    r.addToDom()
+                })
+        })
+
+    }
+
+    static createReview(e){
+        e.preventDefault()
+        let content = document.querySelector("#content").value
+        let beerID = document.querySelector("#beer_id").value
+
+        reviewForm.reset()
+
+        let reviewObj = {
+            content
+        }
+
+        let config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(reviewObj)
+        }
+
+        fetch(`http://localhost:3000/beers/${beerID}/reviews`, config)
+        .then(res => res.json())
+        .then(res => {
+            let r = new Review(res)
+            r.addToDom()
+            newReview.style.display=""
+            reviewForm.style.display="none"
+
+        })
+    }
+
+    static listenForEvents(){
+        newReview.addEventListener('click', this.showForm)
+        reviewForm.addEventListener('submit', (e) => Review.createReview(e))
+    }
+
+    static showForm(){
+        newReview.style.display="none"
+        reviewForm.style.display=""
+    }
+
+    addToDom(){
+        this.reviewsList.appendChild(this.setElementHTML())
+    }
+
+    setElementHTML(){
+         this.element.innerHTML = `
+         <p>${this.content}</p>
+         `   
+         return this.element
+    }
+}
